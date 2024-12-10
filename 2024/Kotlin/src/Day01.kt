f07b16ef6c3e8e3fd5b6bacbfc3374b3bff93d5b
keithkennedy <keith@kennedy.dev>
2024-12-10 18:03:14 +0000

    2024 Day 1 solution

---
+++ 2024/Kotlin/src/Day01.kt
@@ -0,0 +1,22 @@
+import kotlin.math.abs
+
+fun main() {
+    val input = readInput("Day01")
+    val pairs = input.map { line -> line.split("   ")}.map { listOf(it[0].toInt(), it[1].toInt()) }
+    // part 1
+    val orderedLeft = pairs.map { it[0] }.sortedBy { it }
+    val orderedRight = pairs.map { it[1] }.sortedBy { it }
+    val partOneAnswer = List(orderedLeft.size) { index ->
+        abs(orderedLeft[index] - orderedRight[index])
+    }.sum()
+    println(partOneAnswer)
+
+    // part 2
+    val left = pairs.map { it[0] }
+    val right = pairs.map { it[1] }
+    val partTwoAnswer = List(orderedLeft.size) { index ->
+        val thisSide = orderedLeft[index]
+        thisSide * right.count { thisSide == it }
+    }.sum()
+    println(partTwoAnswer)
+}
