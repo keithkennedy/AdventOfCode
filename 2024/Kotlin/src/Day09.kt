5a7686122ca76de0ede8c112f5e9c6a0b31d456d
Keith Kennedy <keith@kennedy.dev>
2024-12-12 22:40:16 +0000

    2024 Day 9 solution (part 2 implemented)

--- 2024/Kotlin/src/Day09.kt
+++ 2024/Kotlin/src/Day09.kt
@@ -1,21 +1,40 @@
 import java.util.concurrent.atomic.AtomicInteger
 
-fun main() {
-    val diskMap = readInput("Day09")[0]
-//    val diskMap = "2333133121414131402"
-    val dilutedDiskMap = dilutedDiskMap(diskMap)
-    val fragmentedDiskMap = fragment(dilutedDiskMap)
-    println(checksum(fragmentedDiskMap))
+class File(val id: Int, val number: Int) {
+    fun isFile(): Boolean = !isSpace()
+    fun isSpace(): Boolean = id == -1
+
+    fun output(): String {
+        if (isFile()) {
+            return id.toString().repeat(number)
+        }
+        return ".".repeat(number)
+    }
 }
 
-fun dilutedDiskMap(diskMap : String) : List<Int> {
+fun output(diskMap: List<File>) : String {
+    return diskMap.joinToString(separator = "", transform = File::output)
+}
+
+fun main() {
+    val diskMap = readInput("Day09").first()
+//    val diskMap = "2333133121414131402"
+//    val dilutedDiskMap = dilutedDiskMap(diskMap)
+//    val fragmentedDiskMap = fragment(dilutedDiskMap)
+    val dilutedDiskMap = dilutedDiskMap2(diskMap)
+    val fragmentedDiskMap = fragment2(dilutedDiskMap)
+    println(checksum2(fragmentedDiskMap))
+}
+
+// Part 1 methods
+fun dilutedDiskMap(diskMap : String) : List<File> {
     val ai = AtomicInteger(0)
     return diskMap.flatMapIndexed { idx, char ->
         if (idx % 2 == 0) {
             val id = ai.getAndIncrement();
-            (0 until char.toString().toInt()).map { id }
+            (0 until char.toString().toInt()).map { File(id, char.toString().toInt()) }
         } else {
-            (0 until char.toString().toInt()).map {  -1 }
+            (0 until char.toString().toInt()).map { File(-1, char.toString().toInt()) }
         }
     }.toList()
 }
@@ -29,88 +48,80 @@ val firstSpaceIndex = diskMap.indexOfFirst { !it.isFile() && size > lastFile.siz
 then swap "space" object (size 3) with file (size 3)
 
  */
-tailrec fun fragment(diskMap: List<Int>) : List<Int> {
-    val firstSpaceIndex = diskMap.indexOfFirst { it == -1 }
-    val lastFileIndex = diskMap.indexOfLast { it != -1 }
+tailrec fun fragment(diskMap: List<File>) : List<File> {
+    val firstSpaceIndex = diskMap.indexOfFirst { it.isSpace() }
+    val lastFileIndex = diskMap.indexOfLast { it.isFile() }
     if (firstSpaceIndex == lastFileIndex-1) {
         return diskMap
     }
 
-    val out = ArrayList<Int>(diskMap)
+    val out = ArrayList<File>(diskMap)
     out[firstSpaceIndex] = diskMap[lastFileIndex]
     out[lastFileIndex] = diskMap[firstSpaceIndex]
     return fragment(out)
 }
 
-fun checksum(fileMap: List<Int>): Long {
-    return fileMap.filter { it != -1 }.mapIndexed { i, c ->
-        (i * c).toLong()
+fun checksum(fileMap: List<File>): Long {
+    return fileMap.filter { it.isFile() }.mapIndexed { i, c ->
+        (i * c.id).toLong()
     }.sum()
 }
 
-//fun _dilutedDiskMap(diskMap: String): List<File> {
-//    val ai = AtomicInteger(0)
-//    return diskMap.mapIndexed { i, c ->
-//        if (i % 2 == 0) {
-//            val id = ai.getAndIncrement();
-//            File(id, c.toString().toInt())
-//        } else {
-//            File(-1, c.toString().toInt())
-//        }
-//    }
-//        .filter { !(it.isSpace() && it.size == 0) }
-//        .toList()
-//}
-//
-//tailrec fun _fragment(diskMap: List<File>) : List<File> {
-//    val firstSpaceIndex = diskMap.indexOfFirst { it.isSpace() }
-//    val lastFileIndex = diskMap.indexOfLast { !it.isSpace() }
-//    if (firstSpaceIndex == lastFileIndex-1) {
-//        return diskMap
-//    }
-//
-//    val out = ArrayList<File>(diskMap)
-//    out[firstSpaceIndex] = diskMap[lastFileIndex]
-//    out[lastFileIndex] = diskMap[firstSpaceIndex]
-//    return _fragment(out)
-//}
-//
-//fun output(diskMap: List<File>) : String {
-//    return diskMap.joinToString("") {
-//        it.output()
-//    }
-//}
-//
-//fun dilutedDiskMap(diskMap: String): String {
-//    val ai = AtomicInteger(0)
-//    return diskMap.mapIndexed { i, c ->
-//        val fileId = if (i % 2 == 0) {
-//            //val x = File(i, c.toString().toInt())
-//            ai.getAndIncrement().toString() // even -> this is a file
-//        } else {
-//            "." // this is free space
-//        }
-//        val size = c.toString().toInt()
-//        fileId.repeat(size)
-//    }.joinToString("")
-//}
-//
-//tailrec fun fragment(diskMap: String) : String {
-//    // left move space
-//    val firstSpaceIndex = diskMap.indexOfFirst { it == '.' }
-//    val lastFileIndex = diskMap.indexOfLast { it != '.' }
-//    val out = diskMap.toCharArray()
-//    out[firstSpaceIndex] = out[lastFileIndex]
-//    out[lastFileIndex] = '.'
-//    if (firstSpaceIndex == lastFileIndex-1) {
-//        return String(out)
-//    }
-//    return fragment(String(out))
-//}
-//
-//fun checksum(fileMap: String) : Int {
-//    // it is not 79669620
-//    return fileMap.filter { it != '.' }.mapIndexed { i, c ->
-//        i * c.toString().toInt()
-//    }.sum()
-//}
+// Part 2 methods
+fun dilutedDiskMap2(diskMap : String) : List<File> {
+    val ai = AtomicInteger(0)
+    return diskMap.mapIndexed { idx, char ->
+        if (idx % 2 == 0) {
+            val id = ai.getAndIncrement();
+            File(id, char.toString().toInt())
+        } else {
+            File(-1, char.toString().toInt())
+        }
+    }.toList()
+}
+
+fun fragment2(diskMap: List<File>) : List<File> {
+    val _diskMap = ArrayList<File>(diskMap)
+
+    val lastFileId = _diskMap.last { it.isFile() }.id
+    for (i in lastFileId downTo 0) {
+        val fileIndex = _diskMap.indexOfFirst { it.isFile() && it.id == i }
+        val file = _diskMap.get(fileIndex)
+        // find the first space that can hold this file based on its size "number"
+        val firstSpaceIndex = _diskMap.indexOfFirst { it.isSpace() && it.number >= file.number }
+        // check the space is "before" the file
+        if (firstSpaceIndex == -1 ||
+            fileIndex < firstSpaceIndex) {
+            continue
+        }
+
+        val space = _diskMap.get(firstSpaceIndex)
+        _diskMap[fileIndex] = space
+        _diskMap[firstSpaceIndex] = file
+
+        if (space.number > file.number){
+            // space needs to be split and then swapped
+            val remainingSpace = File(-1, space.number - file.number)
+            // decrement original space -- file has filled it
+            val originalSpace = File(-1, file.number)
+            _diskMap[fileIndex] = originalSpace
+            _diskMap.add(firstSpaceIndex + 1, remainingSpace)
+        }
+    }
+    return _diskMap
+}
+
+fun checksum2(fileMap: List<File>): Long {
+    var pos = 0L
+    var total = 0L
+    fileMap.forEach { file ->
+        (0 until file.number).forEach { j ->
+            if (file.isFile()) {
+                total += (pos * file.id).toLong()
+            }
+            pos ++
+        }
+    }
+
+    return total
+}
